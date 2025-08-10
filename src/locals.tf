2151858010978e85c2defd6209de9942e947f54d
osagiefe <felix.osagie@hotmail.com>
2025-08-10 15:34:07 -0400

    updated files

--- src/locals.tf
+++ src/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  availability_zones = ["eu-west-2a", "eu-west-2b"]
+  availability_zones = ["us-west-1a", "us-east-1b"]
 
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.0.0/24", "10.0.1.0/24"]
--- src/main.tf
+++ src/main.tf
@@ -2,9 +2,9 @@ terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
   backend "s3" {
-    bucket         = "cc-tf-state-backend-ci-cd"
+    bucket         = "ikeja-q12"
     key            = "tf-infra/terraform.tfstate"
-    region         = "eu-west-2"
+    region         = "us-east-1"
     #dynamodb_table = "terraform-state-locking"
     encrypt        = true
   }
@@ -19,19 +19,19 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
 }
 
 module "tf-state" {
   source      = "./modules/tf-state"
-  bucket_name = "cc-tf-state-backend-ci-cd-100"
+  bucket_name = "c"
 }
 
 module "vpc-infra" {
   source = "./modules/vpc"
 
   # VPC Input Vars
-  vpc_cidr             = local.vpc_cidr
+  vpc_cidr       c-tf-state-backend-ci-cd-100      = local.vpc_cidr
   availability_zones   = local.availability_zones
   public_subnet_cidrs  = local.public_subnet_cidrs
   private_subnet_cidrs = local.private_subnet_cidrs
